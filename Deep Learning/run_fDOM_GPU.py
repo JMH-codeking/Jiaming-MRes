e107fbc06ee9aae1ecb0c3d31f0e6dbe295219e6
Jiaming <zceehub@ucl.ac.uk>
2023-06-01 00:37:23 +0100

    fix: fix bugs for training

--- Deep Learning/run_fDOM_GPU.py
+++ Deep Learning/run_fDOM_GPU.py
@@ -29,7 +29,7 @@ def train_valid(
     else:
         assert 'Wrong Name'
 
-    if torch.backends.mps.is_available():
+    if torch.cuda.is_available():
         device = torch.device('cuda')
     else:
         device = torch.device('cpu')
