639e0a657171d170a6e32de0402040f8c3262e49
Jiaming <zceehub@ucl.ac.uk>
2023-06-06 15:40:58 +0100

    add: new

--- Deep Learning/run_SBRNN.py
+++ Deep Learning/run_SBRNN.py
@@ -146,8 +146,8 @@ class BiLSTM(nn.Module):
     def forward(self, x):
         self.lstm.flatten_parameters()
         seq_len, batch_size, _ = x.size()
-        h0 = torch.randn(2*self.lstm.num_layers, batch_size, self.hidden_size).to(x.device) # 2 for bidirection 
-        c0 = torch.randn(2*self.lstm.num_layers, batch_size, self.hidden_size).to(x.device)
+        h0 = torch.zeros(2*self.lstm.num_layers, batch_size, self.hidden_size).to(x.device) # 2 for bidirection 
+        c0 = torch.zeros(2*self.lstm.num_layers, batch_size, self.hidden_size).to(x.device)
 
         out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
